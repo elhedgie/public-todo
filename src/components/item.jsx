3cf5b47e9c2f651e9c247d9a700f5765ea3f4934
elhedgie <98222227+elhedgie@users.noreply.github.com>
2022-03-02 17:04:33 +0300

    1.0

---
+++ src/components/item.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import './item.css'
+import './innerlist.css'
+import MyBtn from "./button";
+
+export default class MyItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {text: '', done: false, open: false, subtask: [], checked: false}
+        this.doneMaker = this.doneMaker.bind(this)
+        this.innerMaker = this.innerMaker.bind(this)
+        this.changeInput = this.changeInput.bind(this)
+        this.markAsDone = this.markAsDone.bind(this)
+
+    }
+    
+    doneMaker() {
+        // this.setState({done: !this.state.done})
+        this.setState({open: !this.state.open})
+    }
+    changeInput(e) {
+        this.setState({text: e.target.value})
+    }
+    innerMaker(e) {
+        e.preventDefault()
+        this.setState({subtask: [...this.state.subtask, {text: this.state.text}]})
+    }
+    markAsDone() {
+        this.setState({checked: !this.state.checked})
+        console.log(this.state.checked)
+    }
+    render() {
+        return(
+            <li className="item-wrap" >
+                <div className="wrap-todo">
+                    <label className="item-label">
+                        
+                        <input onChange={this.markAsDone} checked={this.state.checked} className="default-radio" type="checkbox"/>
+                        <span className="custom-radio"></span>
+                        <div className="todo-item" onClick={this.doneMaker} key={new Date}>{this.props.item.text}</div>
+                    </label>
+                </div>
+                <div className={this.state.open ? 'open' : 'closed'}>
+                    <form className='inner-form' action="">
+                        <input onChange={this.changeInput}  value={this.state.text} className="inner-input" type="text"/>
+                        <MyBtn onClick={this.innerMaker}></MyBtn>
+                    </form>
+                    <ul className="inner-list">
+                        {
+                            this.state.subtask.map(task=>
+                                <li className="inner-item" key={Math.random()}>{task.text}</li>
+                            )
+                        }
+                    </ul>
+                </div>
+            </li>
+        )
+    }
+}
---
+++ src/components/list.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import MyItem from "./item"
+import './list.css'
+
+export default class MyList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {opened: false, inner: false}
+    }
+    render() {
+        return(
+            <div className="main-list-wrap" >
+                <h1>TODO</h1>
+                 <ul className="wrap-list">
+                    {this.props.items.map(item=> 
+                        <MyItem item={item}></MyItem>
+                        )
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
